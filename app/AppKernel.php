390b2280dbef25f831d8d67bea79db0deb25367c
jjmarquez <jjmarquez@jjmarquez-MN-50>
2016-01-14 19:58:01 -0430

    Merge jjmarquez/master

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
-            new Proyecto\PBundle\ProyectoPBundle(),
+            //new Proyecto\PBundle\ProyectoPBundle(),
             
             new Sonata\CoreBundle\SonataCoreBundle(),
             new Sonata\BlockBundle\SonataBlockBundle(),
--- src/Proyecto/PBundle/ProyectoPBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Proyecto\PBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ProyectoPBundle extends Bundle
-{
-}
